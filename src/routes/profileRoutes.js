b861ffbda60012074bf41e100b6f3d5e16984c37
Nguyễn Tiến Thành <nguyentienthanh.tgdd@gmail.com>
2024-04-22 00:40:00 +0700

    Update: Coordinator and Admin can create user

--- src/routes/profileRoutes.js
+++ src/routes/profileRoutes.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { isAdmin } = require("../middlewares/checkRole");
+const { isAdmin, isCoordinator } = require("../middlewares/checkRole");
 
 const ProfileController = require("../controllers/profileController");
 
 router.get("/delete/:id", isAdmin, ProfileController.delete);
 router.post("/update/:id", ProfileController.update);
-router.post("/create", isAdmin, ProfileController.create);
+router.post("/create", isCoordinator, ProfileController.create);
 router.get("/", ProfileController.index);
 
 module.exports = router;
